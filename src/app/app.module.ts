a9c75f3179d733d3b51f5ce0f777fedaa50d74b3
Karthik Prasad <adkp.mlr@gmail.com>
2019-01-27 01:35:58 +0530

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,56 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, DomSanitizer } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { OverlayContainer } from '@angular/cdk/overlay';
+
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {
+  MatToolbarModule,
+  MatRippleModule,
+  MatIconModule, MatButtonModule, MatMenuModule, MatDividerModule, MatCardModule, MatTabsModule, MatButtonToggleModule, MatIconRegistry
+} from '@angular/material';
+import { CardComponent } from './card/card.component';
+import { ButtonsComponent } from './buttons/buttons.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    ButtonsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FlexLayoutModule,
+    AppRoutingModule,
+    HttpClientModule,
+    MatCardModule, MatTabsModule, FormsModule, MatButtonToggleModule,
+    MatToolbarModule, MatRippleModule, MatIconModule, MatButtonModule, MatMenuModule, MatDividerModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  // constructor(overlayContainer: OverlayContainer) {
+  //   overlayContainer.getContainerElement().classList.add('k-dark');
+  // }
+
+  constructor(
+    private matIconRegistry: MatIconRegistry,
+    private domSanitizer: DomSanitizer
+  ) {
+    this.matIconRegistry.addSvgIcon(
+      'k_icon',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/k_icon.svg')
+    );
+  }
+}
---
+++ src/app/card/card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'k-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css']
+})
+export class CardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
